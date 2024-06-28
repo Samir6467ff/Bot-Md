d3726bfbd03cea8c70c9e03eb8e786df3928542b
Sayed Hamdye <s.sawaza2000@gmail.com>
2024-06-28 12:08:45 +0300

    Update and rename fix-owner-esperando_mensajes.js to المالك-تنظيف.js

--- plugins/المالك-تنظيف.js
+++ plugins/المالك-تنظيف.js
@@ -39,6 +39,6 @@ const handler = async (m, { conn, usedPrefix }) => {
 };
 handler.help = ['del_reg_in_session_owner'];
 handler.tags = ['owner'];
-handler.command = /^(del_reg_in_session_owner|dsowner|clearallsession)$/i;
+handler.command = /^(تنظيف|dsowner)$/i;
 handler.rowner = true
 export default handler;
