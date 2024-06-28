713b6cbfdad00be03e698b96798d65617c319dcb
Sayed Hamdye <s.sawaza2000@gmail.com>
2024-06-28 12:34:29 +0300

    Update and rename downloader-playlist.js to البحث-قائمه يوتيوب.js

--- plugins/البحث-قائمه يوتيوب.js
+++ plugins/البحث-قائمه يوتيوب.js
@@ -47,5 +47,5 @@ ${tradutor.texto2[1]}
 };
 handler.help = ['playlist *<texto>*'];
 handler.tags = ['search'];
-handler.command = /^playlist|playlist2$/i;
+handler.command = /^playlist|يوتيوبي$/i;
 export default handler;
