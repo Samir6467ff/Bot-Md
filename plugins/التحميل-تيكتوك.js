1507e28613f8e3d0fe7ff111e9a7253a00f5d4e8
Sayed Hamdye <s.sawaza2000@gmail.com>
2024-07-03 18:21:55 +0300

    Update and rename descargas-tiktok.js to التحميل-تيكتوك.js

--- plugins/التحميل-تيكتوك.js
+++ plugins/التحميل-تيكتوك.js
@@ -59,7 +59,7 @@ const handler = async (m, {conn, text, args, usedPrefix, command}) => {
     }
   }
 };
-handler.command = /^(tiktok|ttdl|tiktokdl|tiktoknowm|tt|ttnowm|tiktokaudio)$/i;
+handler.command = /^(tiktok|تيك)$/i;
 export default handler;
 
 async function tiktokdlF(url) {
