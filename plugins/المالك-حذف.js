a814ecfbc260a2159a90d91f85e0b8d6644b688c
Sayed Hamdye <s.sawaza2000@gmail.com>
2024-07-04 14:00:04 +0300

    Update and rename owner-delmsg.js to المالك-حذف.js

--- plugins/المالك-حذف.js
+++ plugins/المالك-حذف.js
@@ -6,7 +6,7 @@ const handler = async (m, {command, usedPrefix, text}) => {
   const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
   const tradutor = _translate.plugins.owner_delmsg
 
-  const which = command.replace(/eliminar/i, '');
+  const which = command.replace(/حذف/i, '');
   if (!text) throw `${tradutor.texto1[0]} ${usedPrefix}list${which} ${tradutor.texto1[1]}`;
   const msgs = global.db.data.msgs;
   if (!text in msgs) throw `${tradutor.texto2[0]} '${text}' ${tradutor.texto2[1]}`;
@@ -15,6 +15,6 @@ const handler = async (m, {command, usedPrefix, text}) => {
 };
 handler.help = ['vn', 'msg', 'video', 'audio', 'img', 'sticker'].map((v) => 'del' + v + ' <text>');
 handler.tags = ['database'];
-handler.command = /^eliminar(vn|msg|video|audio|img|sticker)$/;
+handler.command = /^حذف(vn|رساله|فيديو|صوت|صوره|ستيكر)$/;
 handler.rowner = true;
 export default handler;
