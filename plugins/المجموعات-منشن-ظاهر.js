a3709660f759c1238903207a48ac1106c498b528
Sayed Hamdye <s.sawaza2000@gmail.com>
2024-07-04 14:09:27 +0300

    Update and rename gc-tagall.js to المجموعات-منشن-ظاهر.js

--- plugins/المجموعات-منشن-ظاهر.js
+++ plugins/المجموعات-منشن-ظاهر.js
@@ -17,12 +17,12 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   for (const mem of participants) {
     teks += `┣➥ @${mem.id.split('@')[0]}\n`;
   }
-  teks += `*└* 𝐁𝐲 𝐓𝐡𝐞 𝐌𝐲𝐬𝐭𝐢𝐜 - 𝐁𝐨𝐭\n\n*▌│█║▌║▌║║▌║▌║▌║█*`;
+  teks += `*└* 𝐁𝐲 𝐓𝐡𝐞 𝐒𝐇𝐀𝐖𝐀𝐙𝐀-𝐁𝐎𝐓\n\n*▌│█║▌║▌║║▌║▌║▌║█*`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
 handler.tags = ['group'];
-handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i;
+handler.command = /^(منشن)$/i;
 handler.admin = true;
 handler.group = true;
 export default handler;
