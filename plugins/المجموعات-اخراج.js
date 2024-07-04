f9b875ca721bb1e3eb796464d476f27977799a11
Sayed Hamdye <s.sawaza2000@gmail.com>
2024-07-04 14:05:02 +0300

    Update and rename gc-kick2.js to المجموعات-اخراج.js

--- plugins/المجموعات-اخراج.js
+++ plugins/المجموعات-اخراج.js
@@ -14,7 +14,7 @@ const handler = async (m, {conn, participants, usedPrefix, command}) => {
   const owr = m.chat.split`-`[0];
   await conn.groupParticipantsUpdate(m.chat, [user], 'remove');
 };
-handler.command = /^(kick2|echar2|hechar2|sacar2)$/i;
+handler.command = /^(kick2|اخراج)$/i;
 handler.admin = true;
 handler.group = true;
 handler.botAdmin = true;
