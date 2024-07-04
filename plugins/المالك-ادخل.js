bd77640d8713209809a42f109a105ed6da3c5991
Sayed Hamdye <s.sawaza2000@gmail.com>
2024-07-05 00:16:00 +0300

    Update المالك-ادخل.js

--- plugins/المالك-ادخل.js
+++ plugins/المالك-ادخل.js
@@ -1,5 +1,79 @@
+const linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i;
+let enviando;
+const handler = async (m, { conn, text, isMods, isOwner, isPrems }) => {
+  const datas = global;
+
+  if (enviando) return;
+  enviando = true;
+  try {
+    const link = text;
+    if (!link || !link.match(linkRegex)) throw "الرابط غير صالح أو مفقود.";
+    const [_, code] = link.match(linkRegex) || [];
+
+    // الحصول على معلومات المجموعة
+    const groupMetadata = await conn.groupGetInviteInfo(code);
+    const groupImage = await conn.getProfilePicture(groupMetadata.id);
+    const groupDescription = groupMetadata.desc;
+    const groupOwner = groupMetadata.owner;
+    const groupMembers = groupMetadata.participants.length;
+
+    if (isPrems || isMods || isOwner || m.fromMe) {
+      const res = await conn.groupAcceptInvite(code);
+      if (!isOwner) {
+        await conn.groupAdd(m.chat, [m.sender]);
+      }
+      await conn.sendMessage(m.chat, { text: "تم الانضمام إلى المجموعة بنجاح." }, { quoted: m });
+      enviando = false;
+    } else {
+      await conn.sendMessage(m.chat, { text: "ليس لديك الإذن للانضمام إلى المجموعة مباشرة. سيتم إرسال طلب إلى المسؤول." }, { quoted: m });
+      const data = global.owner.filter(([id]) => id)[0];
+      const dataArray = Array.isArray(data) ? data : [data];
+      for (const entry of dataArray) {
+        await conn.sendMessage(
+          entry + '@s.whatsapp.net',
+          {
+            text: `طلب من @${m.sender.split('@')[0]} للانضمام إلى المجموعة:\n\n*—◉ رابط المجموعة:* ${link}\n*—◉ وصف المجموعة:* ${groupDescription}\n*—◉ المنشئ:* @${groupOwner.split('@')[0]}\n*—◉ عدد الأعضاء:* ${groupMembers}`,
+            mentions: [m.sender, groupOwner],
+            contextInfo: {
+              forwardingScore: 9999999,
+              isForwarded: true,
+              mentionedJid: [m.sender, groupOwner],
+              externalAdReply: {
+                showAdAttribution: true,
+                containsAutoReply: true,
+                renderLargerThumbnail: true,
+                title: global.titulowm2,
+                mediaType: 1,
+                thumbnail: groupImage, // استخدام صورة المجموعة هنا
+                mediaUrl: `${link}`,
+                sourceUrl: `${link}`
+              }
+            }
+          },
+          { quoted: m }
+        );
+      }
+      enviando = false;
+    }
+  } catch {
+    enviando = false;
+    throw "حدث خطأ أثناء محاولة الانضمام إلى المجموعة. يرجى المحاولة مرة أخرى لاحقًا.";
+  }
+};
+handler.help = ['join [chat.whatsapp.com]'];
+handler.tags = ['premium'];
+handler.command = /^join|ادخل$/i;
+handler.private = true;
+export default handler;
 
 
+
+
+
+
+
+
+/*
 const linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i;
 let enviando;
 const handler = async (m, {conn, text, isMods, isOwner, isPrems}) => {
@@ -35,3 +109,4 @@ handler.tags = ['premium'];
 handler.command = /^join|ادخل$/i;
 handler.private = true;
 export default handler;
+*/
