0b8e675d7db81a800bbe406aeb9377946364f5f7
Sayed Hamdye <s.sawaza2000@gmail.com>
2024-07-05 02:01:33 +0300

    Update المالك-ادخل.js

--- plugins/المالك-ادخل.js
+++ plugins/المالك-ادخل.js
@@ -1,50 +1,53 @@
-const linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i;
-let enviando;
-const handler = async (m, {conn, text, isMods, isOwner, isPrems, usedPrefix ,command}) => {
-  
+const linkRegex = /chat\.whatsapp\.com\/([0-9A-Za-z]{20,24})/i;
+let enviando = false;
 
- if (enviando) return;
-     enviando = true 
-     m.reply('جاري طلب الانضمام 🧞‍♂️...');
+const handler = async (m, { conn, text, isMods, isOwner, isPrems, usedPrefix, command }) => {
+  if (enviando) return;
+  enviando = true;
+  m.reply('جاري طلب الانضمام 🧞‍♂️...');
+
   try {
-    const link = text //(m.quoted ? m.quoted.text ? m.quoted.text : text : text) || text;
-    if (!link || !link.match(linkRegex)) throw `فين اللينك ي حوب 🧞`;
+    const link = text;
+    if (!link || !link.match(linkRegex)) throw 'فين اللينك ي حوب 🧞';
     const [_, code] = link.match(linkRegex) || [];
 
-// الحصول على معلومات المجموعة
+    // الحصول على معلومات المجموعة
     const groupMetadata = await conn.groupGetInviteInfo(code);
     const groupImage = await conn.getProfilePicture(groupMetadata.id);
     const groupDescription = groupMetadata.desc;
     const groupOwner = groupMetadata.owner;
     const groupMembers = groupMetadata.participants.length;
 
-    const data = global.owner.filter(([id]) => id)[0];
-      const dataArray = Array.isArray(data) ? data : [data];
-      const ownerNumber = dataArray[0];
-    
-    if ( isPrems || isMods || isOwner || m.fromMe) {
-      const res = await conn.groupAcceptInvite(code);
-      await conn.sendMessage(m.chat, {text: `تم الانضمام ي مطوري 🧞‍♂️.`}, {quoted: m})
-      enviando = false 
+    const ownerData = global.owner.filter(([id]) => id)[0];
+    const ownerArray = Array.isArray(ownerData) ? ownerData : [ownerData];
+    const ownerNumber = ownerArray[0];
+
+    if (isPrems || isMods || isOwner || m.fromMe) {
+      await conn.groupAcceptInvite(code);
+      await conn.sendMessage(m.chat, { text: 'تم الانضمام ي مطوري 🧞‍♂️.' }, { quoted: m });
     } else {
-    
-      conn.sendMessage(m.chat, {text:`تم ارسال طلب الانضمام إلي مطوري ${ownerNumber.split('@')[0]} 🧞`}, {quoted: m});
+      conn.sendMessage(m.chat, { text: `تم ارسال طلب الانضمام إلي مطوري ${ownerNumber.split('@')[0]} 🧞` }, { quoted: m });
+
       const msg = `طلب من @${m.sender.split('@')[0]} للانضمام إلى المجموعة:\n\n*—◉ رابط المجموعة:* ${link}\n*—◉ وصف المجموعة:* ${groupDescription}\n*—◉ المنشئ:* @${groupOwner.split('@')[0]}\n*—◉ عدد الأعضاء:* ${groupMembers}`;
-     const buttons = [['انضمــام', `${usedPrefix + command} ${link}`]];
-     
-      for (const entry of dataArray) await conn.sendButton(entry + '@s.whatsapp.net', msg, author, groupImage, buttons, m);
-      
-      //conn.sendMessage(entry + '@s.whatsapp.net', {text: tradutor.texto4 + '@' + m.sender.split('@')[0] + '\n*—◉ Link del grupo:* ' + link, mentions: [m.sender], contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [m.sender], "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm2, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen6, "mediaUrl": `${link}`, "sourceUrl": `${link}`}}}, {quoted: m});
-      enviando = false 
+      const buttons = [['انضمــام', `${usedPrefix + command} ${link}`]];
+
+      for (const entry of ownerArray) {
+        await conn.sendButton(entry + '@s.whatsapp.net', msg, 'author', groupImage, buttons, m);
+      }
     }
   } catch (error) {
-    enviando = false 
-    console.error("خطأ أثناء محاولة الانضمام إلى المجموعة:", error); 
-    throw "خيرها في غيرها جرب تاني ي حوب 🧞.";
+    console.error("خطأ أثناء محاولة الانضمام إلى المجموعة:", error);
+    throw 'خيرها في غيرها جرب تاني ي حوب 🧞.';
+  } finally {
+    enviando = false;
   }
 };
+
 handler.help = ['join [chat.whatsapp.com]'];
 handler.tags = ['premium'];
 handler.command = /^join|ادخل|انضمام$/i;
 handler.private = true;
+
 export default handler;
+
+// By: 𝗦𝗔𝗬𝗘𝗗-𝗦𝗛𝗔𝗪𝗔𝗭𝗔
