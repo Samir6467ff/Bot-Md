9e49d7e36b5f574fd6510f9cd73035a5d21a619b
Sayed Hamdye <s.sawaza2000@gmail.com>
2024-07-04 13:47:58 +0300

    Update owner-exec2.js

--- plugins/owner-exec2.js
+++ plugins/owner-exec2.js
@@ -1,4 +1,4 @@
-import cp, {exec as _exec} from 'child_process';
+/*import cp, {exec as _exec} from 'child_process';
 import {promisify} from 'util';
 
 const exec = promisify(_exec).bind(cp);
@@ -25,3 +25,4 @@ const handler = async (m, {conn, isOwner, command, text, usedPrefix, args, isROw
 handler.customPrefix = /^[$]/;
 handler.command = new RegExp;
 export default handler;
+*/
