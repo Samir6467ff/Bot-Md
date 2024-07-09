c6cf1418e979bcfe4e1963fb82bb036cd661b4f1
Naruto - 2000 <ssawaza30@gmail.com>
2024-07-09 20:26:55 +0300

    Update and rename convertidor-toptt.js to التحويل-لريك.js

---
+++ plugins/التحويل-لريك.js
@@ -0,0 +1,64 @@
+import {toPTT} from '../lib/converter.js';
+
+
+const handler = async (m, {conn, usedPrefix, command}) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const tradutor = _translate.plugins.convertidor_toptt
+
+  const q = m.quoted ? m.quoted : m;
+  const mime = (m.quoted ? m.quoted : m.msg).mimetype || '';
+
+  if (!/video|audio/.test(mime)) throw `*[💾مساعده💾]قم بالرد علي الفيديو او الاغنيه اللي عايز تحولها لريكورد*`;
+  const media = await q.download?.();
+  if (!media && !/video/.test(mime)) throw '*الحجم كبير*';
+  if (!media && !/audio/.test(mime)) throw '*الحجم كبير*';
+  const audio = await toPTT(media, 'mp4');
+  if (!audio.data && !/audio/.test(mime)) throw '*[❗تحذير❗] حصل خطأ*';
+  if (!audio.data && !/video/.test(mime)) throw '*[❗تحذير❗] حصل خطأ*';
+/*
+  if (!/video|audio/.test(mime)) throw `*${tradutor.texto1}*`;
+  const media = await q.download?.();
+  if (!media && !/video/.test(mime)) throw `*${tradutor.texto2}*`;
+  if (!media && !/audio/.test(mime)) throw `*${tradutor.texto3}*`;
+  const audio = await toPTT(media, 'mp4');
+  if (!audio.data && !/audio/.test(mime)) throw `*${tradutor.texto4}*`;
+  if (!audio.data && !/video/.test(mime)) throw `*${tradutor.texto5}*`;
+*/
+  const aa = conn.sendFile(m.chat, audio.data, 'error.mp3', '', m, true, {mimetype: 'audio/mpeg'});
+  if (!aa) return conn.sendMessage(m.chat, {audio: {url: media}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
+};
+handler.help = ['tovn (reply)'];
+handler.tags = ['audio'];
+handler.command = /^لريك|to(vn)$/i;
+export default handler;
+
+
+
+/*
+import {toPTT} from '../lib/converter.js';
+
+const handler = async (m, {conn, usedPrefix, command}) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const tradutor = _translate.plugins.convertidor_toptt
+
+  const q = m.quoted ? m.quoted : m;
+  const mime = (m.quoted ? m.quoted : m.msg).mimetype || '';
+  if (!/video|audio/.test(mime)) throw `*${tradutor.texto1}*`;
+  const media = await q.download?.();
+  if (!media && !/video/.test(mime)) throw `*${tradutor.texto2}*`;
+  if (!media && !/audio/.test(mime)) throw `*${tradutor.texto3}*`;
+  const audio = await toPTT(media, 'mp4');
+  if (!audio.data && !/audio/.test(mime)) throw `*${tradutor.texto4}*`;
+  if (!audio.data && !/video/.test(mime)) throw `*${tradutor.texto5}*`;
+  const aa = conn.sendFile(m.chat, audio.data, 'error.mp3', '', m, true, {mimetype: 'audio/mpeg'});
+  if (!aa) return conn.sendMessage(m.chat, {audio: {url: media}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
+};
+handler.help = ['tovn (reply)'];
+handler.tags = ['audio'];
+handler.command = /^to(vn|(ptt)?)$/i;
+export default handler;
+*/
