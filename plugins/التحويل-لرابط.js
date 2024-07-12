5c2a65460463be07b875f36f0bdc39f1b1e36773
Shawaza2000 <shmdy7938@gmail.com>
2024-07-12 09:47:31 +0300

    Update and rename convertidor-tourl.js to التحويل-لرابط.js

--- plugins/التحويل-لرابط.js
+++ plugins/التحويل-لرابط.js
@@ -3,21 +3,16 @@ import uploadImage from '../lib/uploadImage.js';
 
 
 const handler = async (m) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-  const tradutor = _translate.plugins.convertidor_tourl
-
-  
+ 
   const q = m.quoted ? m.quoted : m;
   const mime = (q.msg || q).mimetype || '';
-  if (!mime) throw `*${tradutor.texto1}*`;
+  if (!mime) throw `*فين الملف إللي عاوز ترفعو ي حوب 🧞‍♂️*`;
   const media = await q.download();
   const isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime);
   const link = await (isTele ? uploadImage : uploadFile)(media);
-  m.reply(`*${tradutor.texto2}* ${link}`);
+  m.reply(`*لينك الملف:* ${link}`);
 };
 handler.help = ['tourl <reply image>'];
 handler.tags = ['sticker'];
-handler.command = /^(upload|tourl)$/i;
+handler.command = /^(لرابط|tourl)$/i;
 export default handler;
