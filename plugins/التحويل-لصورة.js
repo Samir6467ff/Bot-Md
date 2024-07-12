6264f97cb130ad53eaef81bbe191654ed8a611ea
Shawaza2000 <shmdy7938@gmail.com>
2024-07-12 09:52:49 +0300

    Update and rename convertidor-toimg.js to التحويل-لصورة.js

--- plugins/التحويل-لصورة.js
+++ plugins/التحويل-لصورة.js
@@ -2,22 +2,17 @@ import {webp2png} from '../lib/webp2mp4.js';
 
 
 const handler = async (m, {conn, usedPrefix, command}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-  const tradutor = _translate.plugins.convertidor_toimg
-
-
-  const notStickerMessage = `*${tradutor.texto1} ${usedPrefix + command}*`;
+  
+  const notStickerMessage = `*فين الاستيكر اللي عاوز تحولو ي حوب 🧞‍♂️*`;
   if (!m.quoted) throw notStickerMessage;
   const q = m.quoted || m;
   const mime = q.mediaType || '';
-  if (!/sticker/.test(mime)) throw notStickerMessage;
+  if (!/sticker/.test(mime)) throw `*دا مش استيكر ي حوب 🧞‍♂️*`;
   const media = await q.download();
   const out = await webp2png(media).catch((_) => null) || Buffer.alloc(0);
   await conn.sendFile(m.chat, out, 'error.png', null, m);
 };
 handler.help = ['toimg (reply)'];
 handler.tags = ['sticker'];
-handler.command = ['toimg', 'jpg', 'img'];
+handler.command = ['toimg', 'لصورة'];
 export default handler;
