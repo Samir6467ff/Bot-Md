db4dc530ed8c39014094eaa3653cd8e5095beb4f
Shawaza2000 <shmdy7938@gmail.com>
2024-07-15 17:38:37 +0300

    Update and rename herramientas-translate.js to التحويل-ترجم.js

--- plugins/التحويل-ترجم.js
+++ plugins/التحويل-ترجم.js
@@ -12,7 +12,7 @@ const handler = async (m, {args, usedPrefix, command}) => {
   if (!args || !args[0]) return m.reply(msg);
   let lang = args[0];
   let text = args.slice(1).join(' ');
-  const defaultLang = 'es';
+  const defaultLang = 'ar';
   if ((args[0] || '').length !== 2) {
     lang = defaultLang;
     text = args.join(' ');
@@ -32,5 +32,5 @@ const handler = async (m, {args, usedPrefix, command}) => {
     }
   }
 };
-handler.command = /^(translate|traducir|trad)$/i;
+handler.command = /^(ترجمه|ترجم|tr)$/i;
 export default handler;
